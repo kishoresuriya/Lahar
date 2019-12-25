62c67ea2fe31f21213db89bbac41ca07cb428188
kishoresuriya <kishore@sixtyonesteps.com>
2019-12-25 12:03:38 +0530

    form php

--- contact.php
+++ contact.php
@@ -1,88 +1,72 @@
 <?php
 /*
-THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
-*/
-
-require 'PHPMailer-master/PHPMailerAutoload.php';
-
-/*
-*  CONFIGURE EVERYTHING HERE
-*/
+ *  CONFIGURE EVERYTHING HERE
+ */
 
 // an email address that will be in the From field of the email.
-$fromEmail = 'info@sixtyonesteps.com';
+$from = 'Demo contact form <demo@domain.com>';
 
 // an email address that will receive the email with the output of the form
-$sendToEmail = 'kishore@sixtyonesteps.com';
+$sendTo = 'Demo contact form <demo@domain.com>';
 
 // subject of the email
-$subject = 'New Message From Lahar Imports And Exports';
+$subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name -', 'email' => 'Email  -', 'message' => 'Message -');
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'need' => 'Need', 'email' => 'Email', 'message' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
-$okMessage = 'Thank you for contacting us. we will get back to you soon';
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
 /*
-*  LET'S DO THE SENDING
-*/
+ *  LET'S DO THE SENDING
+ */
 
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
 
 try
 {
-    
+
     if(count($_POST) == 0) throw new \Exception('Form is empty');
-    
-$emailTextHtml .= "<h4>Please find the details filled by a website visitor in the contact form of the website</h4><hr><br>";
-    $emailTextHtml .= "<table>";
-    
+            
+    $emailText = "You have a new message from your contact form\n=============================\n";
+
     foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email
+        // If the field exists in the $fields array, include it in the email 
         if (isset($fields[$key])) {
-            $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+            $emailText .= "$fields[$key]: $value\n";
         }
     }
-    $emailTextHtml .= "</table>";
-   
+
+    // All the neccessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
     
-    $mail = new PHPMailer;
-    
-    $mail->setFrom($fromEmail, $fromName);
-    $mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
-    $mail->addReplyTo($from);
-    
-    $mail->isHTML(true);
-    
-    $mail->Subject = $subject;
-    $mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
-    
-    
-    if(!$mail->send()) {
-        throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
-    }
-    
+    // Send email
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
 catch (\Exception $e)
 {
-    // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
 
 // if requested by AJAX request return JSON response
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
-    
+
     header('Content-Type: application/json');
-    
+
     echo $encoded;
 }
 // else just display the message
